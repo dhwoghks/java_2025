4abfca216a8d953b6fefbc8c6e44d414e42e9eaf
Oh jaehwan <111697672+dhwoghks@users.noreply.github.com>
2025-09-15 12:50:51 +0900

    Implement integer sum input in Homework1
    객프 첫 과제

---
+++ Homework1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum=0;
+        System.out.print("정수를 입력하세요: ");
+        int temp = sc.nextInt();
+        sum += temp;
+        System.out.printf("현재까지 입력된 정수의 합은 %d입니다.\n",sum);
+        System.out.print("정수를 입력하세요: ");
+        temp = sc.nextInt();
+        sum += temp;
+        System.out.printf("현재까지 입력된 정수의 합은 %d입니다.\n",sum);
+        System.out.print("정수를 입력하세요: ");
+        temp = sc.nextInt();
+        sum += temp;
+        System.out.printf("현재까지 입력된 정수의 합은 %d입니다.\n",sum);
+        System.out.print("정수를 입력하세요: ");
+        temp = sc.nextInt();
+        sum += temp;
+        System.out.printf("현재까지 입력된 정수의 합은 %d입니다.\n",sum);
+        System.out.print("정수를 입력하세요: ");
+        temp = sc.nextInt();
+        sum += temp;
+        System.out.printf("현재까지 입력된 정수의 합은 %d입니다.\n",sum);
+
+
+    }
+}
